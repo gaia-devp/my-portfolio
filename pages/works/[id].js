e05062a07fc1f23fb3024cf7ba37b5caf3aa2bc0
gaia-devp <m.gaia.pikadev@gmail.com>
2023-04-12 16:07:23 +0900

    first commit

---
+++ pages/works/[id].js
@@ -0,0 +1,37 @@
+import { client } from '../../libs/client';
+import styles from '../../styles/Home.module.scss';
+
+export default function workId({ work }) {
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.title}>{work.title}</h1>
+      <p className={styles.publishedAt}>{work.publishedAt}</p>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: `${work.body}`,
+        }}
+        className={styles.post}
+      />
+    </main>
+  );
+}
+
+// 静的生成のためのパスを指定します
+export const getStaticPaths = async () => {
+  const data = await client.get({ endpoint: "works" });
+
+  const paths = data.contents.map((content) => `/works/${content.id}`);
+  return { paths, fallback: false };
+};
+
+// データをテンプレートに受け渡す部分の処理を記述します
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const data = await client.get({ endpoint: "works", contentId: id });
+
+  return {
+    props: {
+      work: data,
+    },
+  };
+};
