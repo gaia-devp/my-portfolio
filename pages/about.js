60c8c83f94ca056111ee9b96955899046988d387
gaia-devp <m.gaia.pikadev@gmail.com>
2023-05-02 17:27:40 +0900

    commit

--- pages/about.js
+++ pages/about.js
@@ -8,12 +8,12 @@ export default function About() {
       <div class="introduction">
         <div>
           <h3 class="">まつよし がいあ</h3>
-          <p class="">1995年6月9日生まれ　宮崎県宮崎市出身</p>
+          <p class="">1995年6月9日生まれ　<br className="sp_only"></br>宮崎県宮崎市出身</p>
           <p class="">岡山県立大学情報工学部卒</p>
-          <p class="">卒業後は岡山のとある企業でインフラエンジニアとして就職</p>
-          <p class="">コードを書くことの楽しさを知り転職を決意</p>
-          <p class="">宮崎に帰郷するとともに現SaaS企業に就職</p>
-          <p class="">現在は自社システムを用いてWEBサイトを制作しています</p>
+          <p class="">卒業後は岡山のとある企業で<br className="sp_only"></br>インフラエンジニアとして就職</p>
+          <p class="">コードを書くことの楽しさを知り<br className="sp_only"></br>転職を決意</p>
+          <p class="">宮崎に帰郷するとともに<br className="sp_only"></br>現SaaS企業に就職</p>
+          <p class="">現在は自社システムを用いて<br className="sp_only"></br>WEBサイトを制作しています</p>
         </div>
         <img src="/img/dummy.jpg" alt="name" />
       </div>
